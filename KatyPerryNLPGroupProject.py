bc55df974853010ebe668e22e89427477156d2b0
sophia ojegba <roseysofia@yahoo.com>
2023-12-02 15:17:53 -0500

    Testing model and comment

--- KatyPerryNLPGroupProject.py
+++ KatyPerryNLPGroupProject.py
@@ -1,29 +1,27 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 27 00:06:46 2023
-
-@author: becky
-"""
 import numpy as np
 import os
 import pandas as pd
 import nltk
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 # Import file containing data
 path = os.path.dirname(os.path.abspath(__file__))
 filename = "Youtube02-KatyPerry (1).csv"
 fullpath = os.path.join(path, filename)
 katy_perry = pd.read_csv(fullpath, sep=",")
+
 # =============================================================================
 # Data Exploration
 # =============================================================================
 print(katy_perry.head(3))
 print(katy_perry.shape)
 print(katy_perry.info())
+
 # Drop irrelevant columns
 katy_perry = katy_perry[["CONTENT", "CLASS"]]
 print(katy_perry.head(3))
+
 # =============================================================================
 # Data Visualization
 # =============================================================================
@@ -33,39 +31,30 @@ plt.xlabel("0=Non-Spam, 1=Spam")
 plt.ylabel("Count")
 plt.xticks([0, 1])
 plt.show()
-# =============================================================================
-# Pre-processing
-# =============================================================================
 
-import nltk
+# Pre-processing
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 
 
+# =============================================================================
+# Pre-processing
+# =============================================================================
+
 count_vectorizer = CountVectorizer(stop_words="english")
-train_tc = count_vectorizer.fit_transform(katy_perry)
 
-print("\nDimensions of  data:", train_tc.shape)
+# Vectorize and transform training data
+X_train_tc = count_vectorizer.fit_transform(katy_perry["CONTENT"])
+X_train_tfidf = TfidfTransformer().fit_transform(X_train_tc)
 
-tfidf = TfidfTransformer()
-train_tfidf = tfidf.fit_transform(train_tc)
-type(train_tfidf)
+# Model Building & Training
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.naive_bayes import MultinomialNB
 
-# Transform input data using count vectorizer
-input_tc = count_vectorizer.transform(katy_perry)
-type(input_tc)
-print(input_tc)
-# Transform vectorized data using tfidf transformer
-input_tfidf = tfidf.transform(input_tc)
-type(input_tfidf)
-print(input_tfidf)
 
 # =============================================================================
 # Model Building & Training
 # =============================================================================
-from sklearn.model_selection import cross_val_score, KFold
-from sklearn.naive_bayes import MultinomialNB
-
 # Shuffle the dataset
 katy_perry_shuffled = katy_perry.sample(frac=1, random_state=99)
 
@@ -83,7 +72,7 @@ y_train, y_test = y.iloc[:split_index], y.iloc[split_index:]
 
 # Vectorize and transform training data
 X_train_tc = count_vectorizer.transform(X_train)
-X_train_tfidf = tfidf.transform(X_train_tc)
+X_train_tfidf = TfidfTransformer().fit_transform(X_train_tc)
 
 # Fit a Naive Bayes classifier
 naive_bayes_classifier = MultinomialNB()
@@ -101,3 +90,44 @@ print("Cross Validated Accuracy: ", np.mean(cross_val_scores))
 # =============================================================================
 # Model Testing
 # =============================================================================
+
+# Vectorize and transform test data
+X_test_tc = count_vectorizer.transform(X_test)
+X_test_tfidf = TfidfTransformer().fit_transform(X_test_tc)
+
+# Predictions on the test set
+y_pred = naive_bayes_classifier.predict(X_test_tfidf)
+
+# Confusion Matrix
+conf_matrix = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(conf_matrix)
+
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print("\nAccuracy: {:.2%}".format(accuracy))
+
+
+# =============================================================================
+# New comments
+# =============================================================================
+
+new_comments = [
+    "I love Katy Perry's music! 😍",
+    "The concert was amazing, such a great performance!",
+    "Her voice is so beautiful, a true talent.",
+    "Can't wait for the next album to drop!",
+    "👉👉 Free iPhone Giveaway! Click the link now! 📱🆓",
+    "Earn $1000 a day working from home! Check out this amazing opportunity!",
+]
+
+# Vectorize and transform the new comments using the same vectorizer and transformer
+new_comments_tc = count_vectorizer.transform(new_comments)
+new_comments_tfidf = TfidfTransformer().fit_transform(new_comments_tc)
+
+# Predictions on the new comments
+new_comments_predictions = naive_bayes_classifier.predict(new_comments_tfidf)
+
+# Display results
+for comment, prediction in zip(new_comments, new_comments_predictions):
+    print(f"Comment: '{comment}' - Predicted Class: {prediction}")
